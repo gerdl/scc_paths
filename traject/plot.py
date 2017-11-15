21ad8717a6d1dd5b313d343006dc9d48088121c7
Gerd Gruenert <gerd.gruenert@posteo.de>
2017-11-16 00:43:16 +0100

    Complete Turn trajectory

--- traject/plot.py
+++ traject/plot.py
@@ -17,19 +17,9 @@ from traject.scc.turnparams import TurnParams
 # Create a new subplot from a grid of 1x1
 fig, ax = plt.subplots(figsize=(8, 8))
 
-# X = np.linspace(0, 3/2*np.pi, 500, endpoint=True)
-#C, S = scipy.special.fresnel(np.sqrt(2*X/math.pi))
-
-# Plot cosine with a blue continuous line of width 1 (pixels)
-#plt.plot(X, C, color="blue", linewidth=1.0, linestyle="-")
-# Plot sine with a green continuous line of width 1 (pixels)
-#plt.plot(X, S, color="green", linewidth=1.0, linestyle="-")
-#plt.plot(C, S, color="red", linewidth=1.0, linestyle="-")
-
-
 
 tparam = TurnParams(1.0, 1.0)
-turn = Turn(tparam, math.pi*1.9, 1)
+turn = Turn(tparam, math.pi*0.9, 1)
 
 # plot outer circle:
 omega = tparam.omega
@@ -38,6 +28,12 @@ ax.add_patch(Circle(omega, tparam.outer_rad, facecolor='none', edgecolor='black'
 # plot inner circle center point:
 ax.add_patch(Circle(omega, tparam.inner_rad))
 plt.plot(omega[0], omega[1], "x", color='black')
+
+# plot whole line
+XT = np.linspace(0, turn.len, 128, endpoint=True)
+tra = turn.state(XT)
+plt.plot(tra.x, tra.y, color="yellow", linewidth=5.0, linestyle="-")
+
 
 # plot arc segment
 X = np.linspace(0, tparam.len_clothoid_part, 128, endpoint=True)
@@ -61,7 +57,7 @@ plt.plot(turn.state_qg.x, turn.state_qg.y, "bo")
 # plot second clothoid:
 X3 = np.linspace(tparam.len_clothoid_part+turn.len_of_circular_part, 2*tparam.len_clothoid_part+turn.len_of_circular_part, 128, endpoint=True)
 tra = turn.state_clothoid_second(X3)
-plt.plot(tra.x, tra.y, color="red", linewidth=2.0, linestyle="-")
+plt.plot(tra.x, tra.y, color="red", linewidth=1.0, linestyle="-")
 
 # Set x limits, ticks, etc.
 plt.xlim(-4.0, 4.0)
--- traject/scc/turn.py
+++ traject/scc/turn.py
@@ -51,7 +51,36 @@ class Turn(object):
                 the 1-d position along the turn path
 
         """
-        pass
+
+        first_clotho_cond = s < self.params.len_clothoid_part
+        circ_seg_cond = (s > self.params.len_clothoid_part) & \
+                        (s < self.params.len_clothoid_part + self.len_of_circular_part)
+        second_clotho_cond = (s > self.params.len_clothoid_part + self.len_of_circular_part)
+
+        x = np.empty(len(s))
+        y = np.empty(len(s))
+        theta = np.empty(len(s))
+        kappa = np.empty(len(s))
+
+        first_clotho = self.state_clothoid_first(s[first_clotho_cond])
+        x[first_clotho_cond] = first_clotho.x
+        y[first_clotho_cond] = first_clotho.y
+        theta[first_clotho_cond] = first_clotho.theta
+        kappa[first_clotho_cond] = first_clotho.kappa
+
+        circ_seg = self.state_circular(s[circ_seg_cond])
+        x[circ_seg_cond] = circ_seg.x
+        y[circ_seg_cond] = circ_seg.y
+        theta[circ_seg_cond] = circ_seg.theta
+        kappa[circ_seg_cond] = circ_seg.kappa
+
+        second_clotho = self.state_clothoid_second(s[second_clotho_cond])
+        x[second_clotho_cond] = second_clotho.x
+        y[second_clotho_cond] = second_clotho.y
+        theta[second_clotho_cond] = second_clotho.theta
+        kappa[second_clotho_cond] = second_clotho.kappa
+
+        return State(x, y, theta, kappa)
 
     def state_circular(self, s):
         # TODO: Fix the two-clothoids-only case:
@@ -101,22 +130,14 @@ class Turn(object):
         assert ((s >= self.params.len_clothoid_part + self.len_of_circular_part).all())
         assert ((s <= 2*self.params.len_clothoid_part + self.len_of_circular_part).all())
 
-        # inv_clothoid_s = 2 * self.params.len_clothoid_part + self.len_of_circular_part - s
-
-        # calc the original clothoid, then rotate/translate
-        # cloth_state = self.state_clothoid_first(inv_clothoid_s)
-        # cloth_state = cloth_state.rotate_then_translate()
-
         scale = math.sqrt(math.pi / self.params.sigma_max)
 
         # run from len_clothoid_part to 0
         # NOTE: Mathematically, these brackets are nonsense, but in the numpy.array sense, they are absolutely necessary
         inv_clothoid_s = (2 * self.params.len_clothoid_part + self.len_of_circular_part) - s
 
-        # TODO: I think something's still wrong here!
         ssa_csa = scipy.special.fresnel(inv_clothoid_s * math.sqrt(1 / (self.params.delta_min * math.pi)))
 
-        # TODO: I think something's still wrong here!
         # theta changes quadratically with s
         theta = inv_clothoid_s * inv_clothoid_s / (2 * self.params.delta_min)
 
@@ -124,18 +145,13 @@ class Turn(object):
         kappa = inv_clothoid_s / self.params.len_clothoid_part * self.params.kappa_max
 
         state = State(-scale * ssa_csa[1], scale * ssa_csa[0], theta, kappa)
-
         state = state.rotate_then_translate(self.delta, self.state_qg.x,  self.state_qg.y)
-        #state = state.rotate_then_translate(0, -self.params.state_qi.x, -self.params.state_qi.y)
-
-
-        # return state.rotate_then_translate(self.delta, 0, 0)
         return state
 
 
     @cached_property
-    def len_of_turn(self):
-        pass
+    def len(self):
+        return self.len_of_circular_part + 2*self.params.len_clothoid_part
 
     @cached_property
     def len_of_circular_part(self):
@@ -163,18 +179,7 @@ class Turn(object):
     def state_qg(self):
         """The end of the second clothoid"""
 
-        scale = self.params.outer_rad
-        ang = self.delta
-        # x = self.params.omega[0] + scale * (math.sin(self.delta - self.params.gamma) - math.sin(self.params.gamma))
-        # y = self.params.omega[1] + scale * (math.cos(self.params.gamma) - math.cos(self.delta - self.params.gamma))
-
-        #sin_term = math.sin(self.delta - self.params.gamma) - math.sin(self.params.gamma)
-        #cos_term = math.cos(self.params.gamma) - math.cos(self.delta - self.params.gamma)
-
-        #x = self.params.omega[0] * sin_term
-        #y = self.params.omega[1] * cos_term
-
-        st = State(-self.params.omega[0], -self.params.omega[1], 0, 0)
+        st = State(-self.params.omega[0], -self.params.omega[1], self.delta, 0)
         st = st.rotate_then_translate(self.delta+2*self.params.gamma, self.params.omega[0], self.params.omega[1])
 
         return st
